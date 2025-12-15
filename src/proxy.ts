b3db9c51730acabf18477047f33eca42a8f9eea8
tungdo1504vl <tungdo@vulcanlabs.co>
2025-12-15 23:10:28 +0700

    feat: update proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -65,6 +65,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * - public files (public folder)
      */
-    String.raw`/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$).*)`,
+    "/conversations/:path*",
   ],
 };
